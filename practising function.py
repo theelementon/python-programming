ee21069a43a98eeee3afe4d16998d0567800565e
theelementon <vigneshnarenofficial@gmail.com>
2018-08-20 17:54:17 +0530

    "First commit"

---
+++ practising function.py
@@ -0,0 +1,11 @@
+def a_func():   #function name and declaration
+    #inside a function
+    print "this is inside a function."
+a_func()
+
+#function with parameters
+def param(a,b,c,4):
+    print ((a+b+c+d)/4)
+
+param(1,2,3)
+
